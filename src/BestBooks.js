0b27c609a1ebf190854af88d3361908ff0b4052a
Anthony Keith <jt1keith@gmail.com>
2023-03-13 16:57:14 -0700

    front end connected to backend, data sends as intended

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import Slider from 'react-animated-slider';
-import 'react-animated-slider/build/horizontal.css';
+import axios from 'axios';
+const SERVER = process.env.REACT_APP_SERVER;
 
 class BestBooks extends React.Component {
   constructor(props) {
@@ -10,34 +10,39 @@ class BestBooks extends React.Component {
     }
   }
 
+  getBooks = async () => {
+    try {
+      let results = await axios.get(`${SERVER}/books`);
+      this.setState({
+        books: results.data,
+      })
+    } catch (error) {
+      console.log('We have an error:', error.response.data)
+    }
+  }
+
   componentDidMount() {
-    // DONE: Make a GET request to your API to fetch all the books from the database
-    fetch('/api/books')
-      .then(response => response.json())
-      .then(data => this.setState({ books: data }))
-      .catch(error => console.log(error));
+    this.getBooks();
   }
 
   render() {
-    const { books } = this.state;
+    let books = this.state.books.map((book, idx) => (
+      <p key={book._id}>
+        Title: {book.title}, description: {book.description}, status: {book.status}
+      </p>
+    ))
 
     return (
-      <>
-        <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
-
-        {books.length ? (
-          <Slider autoplay={2000}>
-            {books.map((book, index) => (
-              <div key={index} style={{ background: `url(${book.image}) no-repeat center center` }}>
-                <h3>{book.title}</h3>
-                <p>{book.description}</p>
-              </div>
-            ))}
-          </Slider>
+      <main>
+        {this.state.books.length > 0 ? (
+          <>
+            {books}
+          </>
         ) : (
-          <h3>No Books Found :(</h3>
+          <p>No books found.</p>
         )}
-      </>
+
+      </main>
     )
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'react-animated-slider/build/horizontal.css'
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));

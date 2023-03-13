155f21316b15d7f3d14e147000bddb7cf98f9da9
Kameron Watts <kam.watts@gmail.com>
2023-03-13 16:10:14 -0700

    frontend and backend talking

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,42 +1,61 @@
 import React from 'react';
-import Slider from 'react-animated-slider';
-import 'react-animated-slider/build/horizontal.css';
+import axios from 'axios';
+
 
 class BestBooks extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      books: []
+      books: [],
+      title: '',
+      decription: '',
+      image: ''
     }
+  }
+
+
+  getBooks = async () => {
+    try {
+      let results = await axios.get(`${process.env.SERVER}/books`);
+      this.setState({
+        books: results.data
+      })
+    } catch(error) {
+      console.log('An error occurred: ', error.response.data)
+    }
+    console.log(this.state.books);
   }
 
   componentDidMount() {
     // DONE: Make a GET request to your API to fetch all the books from the database
-    fetch('/api/books')
-      .then(response => response.json())
-      .then(data => this.setState({ books: data }))
-      .catch(error => console.log(error));
+    // axios.get('/models/book.js')
+    //   .then(response => response.json())
+    //   .then(data => this.setState({ books: data }))
+    //   .catch(error => console.log(error));
   }
 
   render() {
+    this.getBooks();
     const { books } = this.state;
+    console.log(books);
+    // render the books using the react-animated-slider library
 
     return (
       <>
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
 
-        {books.length ? (
-          <Slider autoplay={2000}>
+        {this.books.length > 0} ? (
+          <div>
             {books.map((book, index) => (
-              <div key={index} style={{ background: `url(${book.image}) no-repeat center center` }}>
-                <h3>{book.title}</h3>
-                <p>{book.description}</p>
+                    <div key={index} style={{ background: `url(${this.book.image}) no-repeat center center` }}>
+                <h3>{this.book.title}</h3>
+                <p>{this.book.description}</p>
               </div>
             ))}
-          </Slider>
+          </div>
         ) : (
-          <h3>No Books Found :(</h3>
-        )}
+          <h3>The collection is empty(</h3>
+        )
       </>
     )
   }

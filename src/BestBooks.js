4debcdd3310791c6aa0879acc69963d54d0d3173
Kameron Watts <kam.watts@gmail.com>
2023-03-13 22:03:17 -0700

    added image carousel with placeholder image

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import Carousel from 'react-bootstrap/Carousel';
 
 const SERVER = process.env.REACT_APP_SERVER;
 
@@ -8,61 +9,61 @@ class BestBooks extends React.Component {
     super(props);
     this.state = {
       books: [],
-      title: '',
-      decription: '',
-      image: ''
+      index: 0 // added index to state
     }
   }
 
+  handleSelect = (selectedIndex) => { // added handleSelect function
+    this.setState({
+      index: selectedIndex
+    });
+  }
 
-  getBooks = async () => {
+  async componentDidMount() { // added async
     try {
-      let results = await axios.get(`${process.env.SERVER}/books`);
+      let results = await axios.get(`${SERVER}/books`);
       this.setState({
         books: results.data
       })
     } catch(error) {
       console.log('An error occurred: ', error.response.data)
     }
-    console.log(this.state.books);
-  }
-
-  getBooks = async () => {
-    try {
-      let results = await axios.get(`${SERVER}/books`);
-      this.setState({
-        books: results.data,
-      })
-    } catch (error) {
-      console.log('We have an error:', error.response.data)
-    }
-  }
-
-  componentDidMount() {
-
-    this.getBooks();
   }
 
   render() {
-    let books = this.state.books.map((book, idx) => (
-      <p key={book._id}>
-        Title: {book.title}, description: {book.description}, status: {book.status}
-      </p>
-    ))
+    const { books, index } = this.state; // destructured state
+    const carouselItems = books.map((book) => (
+      <Carousel.Item key={book._id}>
+        <img
+          className="d-block w-100"
+          src={book.image}
+          alt={book.title}
+        />
+        <Carousel.Caption>
+          <h3>{book.title}</h3>
+          <p>{book.description}</p>
+          <p>Status: {book.status}</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    ));
 
     return (
       <main>
-        {this.state.books.length > 0 ? (
-          <>
-            {books}
-          </>
-        ) : (
-          <p>No books found.</p>
-        )}
-
+        <Carousel activeIndex={index} onSelect={this.handleSelect}>
+          {books.length > 0 ? (
+            carouselItems
+          ) : (
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="https://via.placeholder.com/800x400?text=No+books+found"
+                alt="No books found"
+              />
+            </Carousel.Item>
+          )}
+        </Carousel>
       </main>
-
-    )
+    );
   }
 }
 
